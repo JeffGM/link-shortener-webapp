03e481d235e6137876ebe25f73bbb2fb0386261d
Jefferson <jeovaneml@live.com>
2022-06-03 19:51:44 -0300

    adding mysqldatabase port to the account repository

--- bootstrap.js
+++ bootstrap.js
@@ -2,12 +2,15 @@ import JwtUtils from "./src/application/utils/JwtUtils.js";
 import StringCryptoUtils from "./src/application/utils/StringCryptUtils.js"
 import AccountService from "./src/application/domain/Account/AccountService.js";
 import AccountRepositoryAdapter from "./src/infrastructure/AccountRepositoryAdapter.js";
+import MysqlDatabasePort from "./src/application/port/MysqlDatabasePort.js";
 import bcrypt from 'bcrypt';
+import mysql from 'mysql';
 
+let mysqlDatabasePort = new MysqlDatabasePort(mysql, "localhost", "root", "admin", "pds", 3306);
 let jwtUtils = new JwtUtils("my-secret-pass");
 let stringCryptoUtils = new StringCryptoUtils(bcrypt);
 
-let accountRepositoryAdapter = new AccountRepositoryAdapter();
+let accountRepositoryAdapter = new AccountRepositoryAdapter(mysqlDatabasePort);
 let accountService = new AccountService(accountRepositoryAdapter, stringCryptoUtils, accountRepositoryAdapter);
 
 let dependencyContainer = {
