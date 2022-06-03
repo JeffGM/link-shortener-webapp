d33f204de0bae3ce9604fc829cab97821f6b63d9
Jefferson <jeovaneml@live.com>
2022-06-03 16:21:35 -0300

    added login functionality

--- src/application/domain/Account/AccountService.js
+++ src/application/domain/Account/AccountService.js
@@ -1,9 +1,10 @@
 import Account from "Account.js";
 
 class AccountService {
-    constructor(accountRepositoryAdapter, stringEncryptor) {
+    constructor(accountRepositoryAdapter, stringEncryptorUtil, jwtUtil) {
         this.accountRepositoryAdapter = accountRepositoryAdapter;
-        this.stringEncryptor = stringEncryptor;
+        this.stringEncryptor = stringEncryptorUtil;
+        this.jwtUtil = jwtUtil;
     }
 
     createAccount(email, password) {
@@ -12,9 +13,32 @@ class AccountService {
             throw new Error("Invalid email!")
         }
 
-        let encryptedPassword = this.stringEncryptor.encrypt(password)
+        let encryptedPassword = this.stringEncryptorUtil.encrypt(password)
         let newAccount = new Account(email, encryptedPassword);   
         this.accountRepositoryAdapter.saveAccount(newAccount);
+    }
+
+    login(email, password) {
+        this.validateLoginParams(email, password);
+        let encryptedPassword = this.stringEncryptorUtil.encrypt(password);
+
+        let loggedAccount = this.accountRepositoryAdapter.getAccount(email, encryptedPassword);
+
+        if (!loggedAccount) {
+            throw new Error("No account found with the specified credentials!");
+        }
+
+        return this.jwtUtil.getTokenForAccount(email);
+    }
+
+    validateLoginParams(email, password) {
+        if (!email) {
+            throw new Error("An email must be informed to log in!");
+        }
+
+        if (!password) {
+            throw new Error("A password must be informed to log in!");
+        }
     }
 
     validateEmail(email) {
