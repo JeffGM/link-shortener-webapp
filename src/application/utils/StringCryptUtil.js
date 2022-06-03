21739f28c77c40157138a8ec7a26eee3fad9cc70
Jefferson <jeovaneml@live.com>
2022-06-03 16:48:08 -0300

    added string crypto util class

---
+++ src/application/utils/StringCryptUtil.js
@@ -0,0 +1,26 @@
+class StringCryptUtil {
+    constructor(bcrypt) {
+        this.bcrypt = bcrypt;
+        this.saltRounds = 10;
+
+    } 
+
+    encrypt(stringToEncrypt) {
+        return await this.bcrypt.hash(stringToEncrypt, this.saltRounds, function(err, hash) {
+            if (err) {
+              throw new Error("An error ocurred while hashing the string!");
+            }
+            return hash;
+        });
+    }
+
+    compare(stringToCheck, hashedString) {
+        return await bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
+            if (err) {
+                throw new Error("An error ocurred while comparing the strings!");
+            }
+
+            return result;
+        });
+    }
+}
