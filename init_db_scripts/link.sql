2aafa4a4a2499b5f18ec582c2ab9f0b1547885c8
Gabriel Tonioni Duarte <gtonioni13@gmail.com>
2022-06-04 15:16:53 -0300

    added link table to database

---
+++ init_db_scripts/link.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `link` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `originalUrl` varchar(500) NOT NULL UNIQUE,
+  `shortenedUrl` varchar(500) NOT NULL UNIQUE,
+  `owner` varchar(45) NOT NULL,
+  `activated` varchar(5) NOT NULL,
+  `numberOfClicks` int DEFAULT NULL,
+  `profit` decimal(19, 4) DEFAULT NULL,
+  `ad` blob DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `expirationDate` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
