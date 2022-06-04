b2de4f2fd8cd1aeeea56f2e35ac28eca6baca08c
Gabriel Tonioni Duarte <gtonioni13@gmail.com>
2022-06-04 15:03:04 -0300

    link shortening working

---
+++ src/application/domain/LinkAggregate/Link.js
@@ -0,0 +1,64 @@
+import LinkConfig from "./LinkConfig.js"
+import LinkStatistics from "./LinkStatistic.js"
+
+// Aggregate's root
+export default class Link {
+    constructor(originalUrl, shortenedUrl, owner) {
+        this.originalUrl = originalUrl;
+        this.shortenedUrl = shortenedUrl;
+        this.owner = owner;
+        this.activated = true;
+        this.stats = new LinkStatistics();
+        this.config = new LinkConfig();
+    }
+
+    getOriginalUrl() {
+        return this.originalUrl;
+    }
+
+    getShortenedUrl() {
+        return this.shortenedUrl;
+    }
+
+    getOwner() {
+        return this.owner;
+    }
+
+    getActivated() {
+        return this.activated;
+    }
+
+    deactivate() {
+        this.activated = false;
+    }
+
+    getStats() {
+        return this.stats;
+    }
+
+    getConfig() {
+        return this.config;
+    }
+
+    updateStats(numberOfClicks, profit) {
+        this.stats = new LinkStatistics(numberOfClicks, profit);
+    }
+
+    updateConfig(ad, password, expirationDate) {
+        this.config = new LinkConfig(ad, password, expirationDate);
+    }
+
+    getSerialized() {
+        return {
+            originalUrl: this.getOriginalUrl(),
+            shortenedUrl: this.getShortenedUrl(),
+            owner: this.getOwner(),
+            activated: this.getActivated(),
+            numberOfClicks: this.getStats().getNumberOfClicks(),
+            profit: this.getStats().getProfit(),
+            ad: this.getConfig().getAd(),
+            password: this.getConfig().getPassword(),
+            expirationDate: this.getConfig().getExpirationDate()
+        }
+    }
+}
