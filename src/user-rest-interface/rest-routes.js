f91260e2ac7aa7aecfa0afcf42093ac4f20bcc4c
Gabriel Tonioni Duarte <gtonioni13@gmail.com>
2022-06-04 15:11:34 -0300

    added create link route

--- src/user-rest-interface/rest-routes.js
+++ src/user-rest-interface/rest-routes.js
@@ -1,5 +1,7 @@
 import AccountAdapter from "./AccountAdapter.js";
+import LinkAdapter from "./LinkAdapter.js";
 
 export default function(app, dependencyContainer) {
     app.post('/account', (req, res) => AccountAdapter.createAccount(req, res, dependencyContainer))
+    app.post('/link', (req, res) => LinkAdapter.createLink(req, res, dependencyContainer))
 }
