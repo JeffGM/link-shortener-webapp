02aecad98a9fa32f9301ca17ef781e9a07ef100d
Gabriel Tonioni Duarte <gtonioni13@gmail.com>
2022-06-05 19:22:53 -0300

    added route for adding expiration date to link

--- src/user-rest-interface/rest-routes.js
+++ src/user-rest-interface/rest-routes.js
@@ -5,4 +5,5 @@ export default function(app, dependencyContainer) {
     app.post('/account', (req, res) => AccountAdapter.createAccount(req, res, dependencyContainer))
     app.post('/link', (req, res) => LinkAdapter.createLink(req, res, dependencyContainer))
     app.post('/password', (req, res) => LinkAdapter.addLinkPassword(req, res, dependencyContainer))
+    app.post('/expiration', (req, res) => LinkAdapter.addLinkExpirationDate(req, res, dependencyContainer))
 }
