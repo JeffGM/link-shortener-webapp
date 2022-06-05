ec6846318a47aaf5d4bf90244e322c64f187a6aa
Gabriel Tonioni Duarte <gtonioni13@gmail.com>
2022-06-05 15:03:08 -0300

    added adapter for handling request to add link password

--- src/user-rest-interface/LinkAdapter.js
+++ src/user-rest-interface/LinkAdapter.js
@@ -12,4 +12,19 @@ export default class LinkAdapter {
 
         return res.send(200, "Success! -> " + shortenedUrl);
     }
+
+    static addLinkPassword(req, res, dependencyContainer) {
+        let owner = req.body.username;
+        let shortenedUrl = req.body.url;
+        let password = req.body.password;
+
+        try {
+            dependencyContainer["linkService"].addLinkPassword(owner, shortenedUrl, password);
+            console.log("Returned from addLinkPassword service");
+        } catch(err) {
+            return res.status(422).send(err.message);
+        }
+
+        return res.send(200, "Success!");
+    }
 }
