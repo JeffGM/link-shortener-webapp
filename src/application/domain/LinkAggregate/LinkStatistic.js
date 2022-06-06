fd8bb486ec0d280b394a746d0f4835a1011815b7
Gabriel Tonioni Duarte <gtonioni13@gmail.com>
2022-06-05 21:58:52 -0300

    minor changes to link aggregate

--- src/application/domain/LinkAggregate/LinkStatistic.js
+++ src/application/domain/LinkAggregate/LinkStatistic.js
@@ -1,5 +1,5 @@
 export default class LinkStatistics {
-    constructor(numberOfClicks = 0, profit = 0) {
+    constructor({numberOfClicks = 0, profit = 0}) {
         this.numberOfClicks = numberOfClicks;
         this.profit = profit;
     }
